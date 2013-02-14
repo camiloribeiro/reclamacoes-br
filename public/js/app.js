67e23441f9210e5663ca4cc444db0f42f20bf353
chiconato <cachiconato@gmail.com>
2013-02-14 15:01:08 -0200

    Adicionando spinner no laading dos tipos de reclamações

--- public/js/app.js
+++ public/js/app.js
@@ -66,6 +66,26 @@ function AnaliseCtrl($scope, $http) {
 }
 
 function AnaliseGrupoCtrl($scope, $routeParams, $http) {
+  var opts = {
+    lines: 9, // The number of lines to draw
+    length: 10, // The length of each line
+    width: 8, // The line thickness
+    radius: 17, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  var target = document.getElementById('spinner');
+  var spinner = new Spinner(opts).spin(target);
+  $scope.loaded = false;
   $scope.grupo = {};
   $scope.empresas = [];
 
@@ -94,6 +114,7 @@ function AnaliseGrupoCtrl($scope, $routeParams, $http) {
       $scope.empresas = data;
 
       $http.get('/reclamacoes/'+$scope.grupo.id).success(function(data) {
+        $scope.loaded = true;
         $scope.reclamacoes = data;
 
         var data = new google.visualization.DataTable();
