1393ec888eac15b0ab1d6adf3dfb1078e1db7d43
chiconato <cachiconato@gmail.com>
2013-02-18 23:02:06 -0300

    Adicionando classe ao config.ru

---
+++ app/model/top_problems.rb
@@ -0,0 +1,32 @@
+class TopProblems
+  include MongoMapper::Document
+  set_collection_name :top_problems
+  
+  key :problema, String
+  key :total, Integer
+  
+  def self.map
+    <<-MAP
+    function() {
+      var empresa = db.empresas.findOne({_id: this.empresa_id});
+      emit({_id: empresa.group_id, problema: this.problema}, {total: 1});
+    }
+    MAP
+  end
+  
+  def self.reduce
+    <<-REDUCE
+    function(key, values) { 
+      var result = {total: 0};
+      values.forEach(function(doc) {
+        result.total += doc.total;
+      });
+      return result;
+    } 
+    REDUCE
+  end
+
+  def self.build
+    Reclamacao.collection.map_reduce(map, reduce, :out => 'top_problems')
+  end
+end
--- config.ru
+++ config.ru
@@ -7,6 +7,7 @@ require './app/model/reclamacao'
 require './app/model/consumidor'
 require './app/model/empresa' 
 require './app/model/empresa_stats'
+require './app/model/top_problems'
 
 ENV['MONGODB_URI'] = 'mongodb://localhost:27017/dev' unless ENV['MONGODB_URI']
 
