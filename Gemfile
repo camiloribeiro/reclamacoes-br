78598815827a6dee46f9cbf4c8282b090d815512
chiconato <cachiconato@gmail.com>
2013-02-22 00:23:28 -0300

    Caching some requests to increase performance.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'mongo_mapper'
 gem 'bson_ext'
+gem 'rack-cache'
 gem 'sinatra'
 
 group :development do
--- reclamacoes_app.rb
+++ reclamacoes_app.rb
@@ -1,4 +1,6 @@
 class ReclamacoesApp < Sinatra::Base
+  use Rack::Cache
+
   configure do
     set :views, Proc.new { File.join(root, "public/views") }
     MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
@@ -16,6 +18,7 @@ class ReclamacoesApp < Sinatra::Base
   end
 
   get '/empresas/stats/:ano' do
+    cache_control :public, :max_age => 36000
     EmpresaStats.where('_id.ano' => params[:ano].to_i).sort(:'value.total'.desc).limit(20).all.to_json
   end
 
@@ -28,15 +31,17 @@ class ReclamacoesApp < Sinatra::Base
   end
 
   get '/groups/:id/empresas' do
+    cache_control :public, :max_age => 36000
     Empresa.by_group(params[:id].to_i).to_json
   end
 
   get '/groups/:id/reclamacoes' do
-    #Empresa.reclamacoes_by_group(params[:id].to_i).to_json
+    cache_control :public, :max_age => 36000
     TopProblems.where('_id._id' => params[:id].to_i).sort(:'value.total'.desc).limit(5).to_json
   end
 
   get '/groups/:id/:ano' do
+    cache_control :public, :max_age => 36000
     EmpresaStats.where('_id.grupo' => params[:id].to_i, '_id.ano'=> params[:ano].to_i).first.to_json
   end
   
@@ -47,14 +52,17 @@ class ReclamacoesApp < Sinatra::Base
   end
 
   get '/estados/stats' do
+    cache_control :public, :max_age => 36000
     stats = EstadoStats.sort(:_id).all.to_json
   end
 
   get '/reclamantes/genero/:ano' do
+    cache_control :public, :max_age => 36000
     ReclamantesGenero.where('_id.ano' => params[:ano].to_i).to_json
   end
 
   get '/reclamantes/idade/:ano' do
+    cache_control :public, :max_age => 36000
     ReclamantesIdade.where('_id.ano' => params[:ano].to_i).to_json
   end
 end
